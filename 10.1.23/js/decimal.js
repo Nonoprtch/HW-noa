c6273c5da59139fb4aacf7b8ca2beb3ade9a2d8f
Nonoprtch <120499463+Nonoprtch@users.noreply.github.com>
2023-01-10 13:20:22 +0200

    10.1.23

---
+++ 10.1.23/js/decimal.js
@@ -0,0 +1,5 @@
+var num
+var dec = num % 1;
+var whole = num - dec;
+var message = "The whole number is " + whole + " and the decimal is " + dec
+aler(message)
---
+++ 10.1.23/js/main.js
@@ -0,0 +1,6 @@
+var num = a , b , c;
+var moyenne = somme /3;
+var somme = prompt ("Enter your 3 number");
+var moyenne = prompt ("Enter the somme of the 3 numbers and split it to 3");
+var message = "Your somme is " + somme + " and your moyenne is " + moyenne
+alert(message)
